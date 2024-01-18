9d306389eb04475016606fb8e04173317ec3d780
IshwaryaSriram <ishwaryasriram@live.com>
2024-01-18 14:53:05 +0530

    updating remote

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { logInRouter } from './routes/login';
 import { logOutRouter } from './routes/logout';
 import { signUpRouter } from './routes/signup';
+
+import { User } from './models/user';
 
 import { errorHandler } from './middlewares/error-handler';
 import { InvalidRouteError } from './errors/invalid-route-error';
@@ -24,6 +27,16 @@ app.all('*', async (req,res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log("Listening on port: 3000");
-})
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        
+        app.listen(3000, () => {
+            console.log("Listening on port: 3000");
+        })
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+start();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,9 @@ import { ValidationError, body, validationResult } from 'express-validator';
 
 import { RequestValidationError } from "../errors/request-validation-error";
 import { DatabaseCOnnectionError } from "../errors/database-connection-errors";
+import { BadRequestError } from "../errors/bad-request-error";
+
+import { User } from "../models/user";
 
 const router = express.Router();
 
@@ -18,15 +21,23 @@ router.post('/api/users/signup' , [
 async (req: Request,res: Response) => {
     const errors = validationResult(req);
 
-    // console.log(errors);
     if(!errors.isEmpty()) {
         throw new RequestValidationError(errors.array());
     }
+
     const { email, password } = req.body;
+    
+    const existingUser = await User.findOne({ email });
+    if(existingUser) {
+        throw new BadRequestError("Email in use");
+    }
 
-    console.log("Creating a user");
+    const newUser = User.build({ email, password });
+    await newUser.save();
+    
+    res.status(201).send(newUser);
 
-    // throw new DatabaseCOnnectionError();
+
     res.send({}); 
 
 
